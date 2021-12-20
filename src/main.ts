5f92daac22d5b2e0354f5d699f67eafa128895bd
Florian Timm <FlorianTimm@github.com>
2021-12-20 15:09:36 +0100

    Optimierung

--- src/main.ts
+++ src/main.ts
@@ -8,279 +8,25 @@ import TileWMS from 'ol/source/TileWMS.js';
 import {
   fromLonLat
 } from 'ol/proj.js';
+import TileSource from 'ol/source/Tile';
+import layerUrlsJson from './layer.json'
 
-let layer = [
-  new TileLayer({
-    title: "Geobasis GB",
-    opacity: 0.3,
-    source: new TileWMS({
-      url: 'https://geodienste.hamburg.de/HH_WMS_Geobasiskarten_GB?',
-      params: {
-        'LAYERS': 'M100000_graublau,M60000_graublau,M20000_graublau,M5000_graublau,M125000_graublau,M40000_graublau,M10000_graublau,M2500_graublau',
-        'TILED': true
-      },
-      serverType: 'geoserver',
-      // Countries have transparency, so do not fade tiles:
-      transition: 0
-    })
-  }),
-  new TileLayer({
-    title: "1880-1889",
-    visible: false,
-    source: new TileWMS({
-      url: 'https://geodienste.hamburg.de/HH_WMS_Vermessungskarten_1880_1925?',
-      params: {
-        'LAYERS': 'vermessungskarten_1879_89',
-        'TILED': true
-      },
-      serverType: 'geoserver',
-      // Countries have transparency, so do not fade tiles:
-      transition: 0
-    })
-  }),
-  new TileLayer({
-    title: "1890-1899",
-    visible: false,
-    source: new TileWMS({
-      url: 'https://geodienste.hamburg.de/HH_WMS_Vermessungskarten_1880_1925?',
-      params: {
-        'LAYERS': 'vermessungskarten_1890_99',
-        'TILED': true
-      },
-      serverType: 'geoserver',
-      // Countries have transparency, so do not fade tiles:
-      transition: 0
-    })
-  }),
+let layerUrls: { name: string, opacity?: number, url: string, layer: string, visible?: boolean }[] = layerUrlsJson;
 
-  new TileLayer({
-    title: "1900-1910",
-    visible: false,
+let layer:TileLayer<TileSource>[] = [];
+layerUrls.forEach((values) => {
+  layer.push(new TileLayer({
+    opacity: values.opacity??1,
+    visible: values.visible??false,
     source: new TileWMS({
-      url: 'https://geodienste.hamburg.de/HH_WMS_Vermessungskarten_1880_1925?',
+      url: values.url,
       params: {
-        'LAYERS': 'vermessungskarten_1900_10',
+        'LAYERS': values.url,
         'TILED': true
-      },
-      serverType: 'geoserver',
-      // Countries have transparency, so do not fade tiles:
-      transition: 0
+      }
     })
-  }),
-
-  new TileLayer({
-    title: "1910-1915",
-    visible: false,
-    source: new TileWMS({
-      url: 'https://geodienste.hamburg.de/HH_WMS_Vermessungskarten_1880_1925?',
-      params: {
-        'LAYERS': 'vermessungskarten_1910_15',
-        'TILED': true
-      },
-      serverType: 'geoserver',
-      // Countries have transparency, so do not fade tiles:
-      transition: 0
-    })
-  }),
-
-  new TileLayer({
-    title: "1916-1925",
-    visible: false,
-    source: new TileWMS({
-      url: 'https://geodienste.hamburg.de/HH_WMS_Vermessungskarten_1880_1925?',
-      params: {
-        'LAYERS': 'vermessungskarten_1816_25',
-        'TILED': true
-      },
-      serverType: 'geoserver',
-      // Countries have transparency, so do not fade tiles:
-      transition: 0
-    })
-  }),
-
-  new TileLayer({
-    title: "1925-1930",
-    visible: false,
-    source: new TileWMS({
-      url: 'https://geodienste.hamburg.de/HH_WMS_Historische_Karte_1_5000?',
-      params: {
-        'LAYERS': 'Jahrgang_1925-1930',
-        'TILED': true
-      },
-      serverType: 'geoserver',
-      // Countries have transparency, so do not fade tiles:
-      transition: 0
-    })
-  }),
-  new TileLayer({
-    title: "1930-1940",
-    visible: false,
-    source: new TileWMS({
-      url: 'https://geodienste.hamburg.de/HH_WMS_Historische_Karte_1_5000',
-      params: {
-        'LAYERS': 'Jahrgang_1930-1940',
-        'TILED': true
-      },
-      serverType: 'geoserver',
-      // Countries have transparency, so do not fade tiles:
-      transition: 0
-    })
-  }),
-  new TileLayer({
-    title: "1940-1950",
-    visible: false,
-    source: new TileWMS({
-      url: 'https://geodienste.hamburg.de/HH_WMS_Historische_Karte_1_5000',
-      params: {
-        'LAYERS': 'Jahrgang_1940-1950',
-        'TILED': true
-      },
-      serverType: 'geoserver',
-      // Countries have transparency, so do not fade tiles:
-      transition: 0
-    })
-  }),
-  new TileLayer({
-    title: "1950-1960",
-    visible: false,
-    source: new TileWMS({
-      url: 'https://geodienste.hamburg.de/HH_WMS_Historische_Karte_1_5000',
-      params: {
-        'LAYERS': 'Jahrgang_1950-1960',
-        'TILED': true
-      },
-      serverType: 'geoserver',
-      // Countries have transparency, so do not fade tiles:
-      transition: 0
-    })
-  }),
-  new TileLayer({
-    title: "1960-1970",
-    visible: false,
-    source: new TileWMS({
-      url: 'https://geodienste.hamburg.de/HH_WMS_Historische_Karte_1_5000',
-      params: {
-        'LAYERS': 'Jahrgang_1960-1970',
-        'TILED': true
-      },
-      serverType: 'geoserver',
-      // Countries have transparency, so do not fade tiles:
-      transition: 0
-    })
-  }),
-  new TileLayer({
-    title: "1970-1980",
-    visible: false,
-    source: new TileWMS({
-      url: 'https://geodienste.hamburg.de/HH_WMS_Historische_Karte_1_5000',
-      params: {
-        'LAYERS': 'Jahrgang_1970-1980',
-        'TILED': true
-      },
-      serverType: 'geoserver',
-      // Countries have transparency, so do not fade tiles:
-      transition: 0
-    })
-  }),
-  new TileLayer({
-    title: "1980-1990",
-    visible: false,
-    source: new TileWMS({
-      url: 'https://geodienste.hamburg.de/HH_WMS_Historische_Karte_1_5000',
-      params: {
-        'LAYERS': 'Jahrgang_1980-1990',
-        'TILED': true
-      },
-      serverType: 'geoserver',
-      // Countries have transparency, so do not fade tiles:
-      transition: 0
-    })
-  }),
-  new TileLayer({
-    title: "1990-2000",
-    visible: false,
-    source: new TileWMS({
-      url: 'https://geodienste.hamburg.de/HH_WMS_Historische_Karte_1_5000',
-      params: {
-        'LAYERS': 'Jahrgang_1990-2000',
-        'TILED': true
-      },
-      serverType: 'geoserver',
-      // Countries have transparency, so do not fade tiles:
-      transition: 0
-    })
-  }),
-  new TileLayer({
-    title: "2005",
-    visible: false,
-    source: new TileWMS({
-      url: 'https://geodienste.hamburg.de/HH_WMS_Historische_Karte_1_5000',
-      params: {
-        'LAYERS': 'Jahrgang_2005',
-        'TILED': true
-      },
-      serverType: 'geoserver',
-      // Countries have transparency, so do not fade tiles:
-      transition: 0
-    })
-  }),
-  new TileLayer({
-    title: "2010",
-    visible: false,
-    source: new TileWMS({
-      url: 'https://geodienste.hamburg.de/HH_WMS_Historische_Karte_1_5000',
-      params: {
-        'LAYERS': 'Jahrgang_2010',
-        'TILED': true
-      },
-      serverType: 'geoserver',
-      // Countries have transparency, so do not fade tiles:
-      transition: 0
-    })
-  }),
-  new TileLayer({
-    title: "2015",
-    visible: false,
-    source: new TileWMS({
-      url: 'https://geodienste.hamburg.de/HH_WMS_Historische_Karte_1_5000',
-      params: {
-        'LAYERS': 'Jahrgang_2015',
-        'TILED': true
-      },
-      serverType: 'geoserver',
-      // Countries have transparency, so do not fade tiles:
-      transition: 0
-    })
-  }),
-  new TileLayer({
-    title: "2020",
-    visible: false,
-    source: new TileWMS({
-      url: 'https://geodienste.hamburg.de/HH_WMS_Historische_Karte_1_5000',
-      params: {
-        'LAYERS': 'Jahrgang_2020',
-        'TILED': true
-      },
-      serverType: 'geoserver',
-      // Countries have transparency, so do not fade tiles:
-      transition: 0
-    })
-  }),
-  new TileLayer({
-    title: "aktuell",
-    visible: true,
-    source: new TileWMS({
-      url: 'https://geodienste.hamburg.de/HH_WMS_DK5',
-      params: {
-        'LAYERS': 'DK5',
-        'TILED': true
-      },
-      serverType: 'geoserver',
-      // Countries have transparency, so do not fade tiles:
-      transition: 0
-    })
-  }),
-];
+  }))
+})
 
 const map = new Map({
   target: 'map',
@@ -296,6 +42,7 @@ document.getElementById("karte").addEventListener("change", function (e) {
     ele.setVisible(false);
   });
   layer[0].setVisible(true);
-  layer[e.target.value].setVisible(true);
-  document.getElementById("ueberschrift").innerHTML = layer[e.target.value].get("title");
+  let id = (<HTMLInputElement>document.getElementById("karte")).value
+  layer[id].setVisible(true);
+  document.getElementById("ueberschrift").innerHTML = layerUrls[id].get("name");
 });
